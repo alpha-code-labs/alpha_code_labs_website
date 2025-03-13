0ff97dab3fcea7736d1f0047c13ad46ee92c8693
alpha-code-labs <sandeep.nair@alphacodelabs.com>
2025-03-13 23:06:10 +0530

    updated clarity integration

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", ${process.env.NEXT_PUBLIC_CLARITY});
+              })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY}");
             `,
           }}
         />
@@ -71,7 +71,7 @@ export default function RootLayout({ children }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
             `,
           }}
         />
